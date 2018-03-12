dcf8074f02e25b23fc5da61d0a9859d0c984ae78
TauWoo <tauwoo@seuxw.cc>
2018-03-12 18:16:55 +0800

    bugfix

--- auto_post_main.py
+++ auto_post_main.py
@@ -15,22 +15,20 @@ def user_cmd(opt_id):
         if opt_id == 3:
             pwd = user.get_user_password(username)
             print("用户【%s】的信息如下\n密码：%s\n用户类型：%s"%(pwd[0],pwd[1],user_type_dict[str(pwd[2])]))
-            return
-
-        password = input("请输入需要%s操作的密码\n"%opt)
-        usertype = int(input("请输入需要%s操作的用户类型 1-只有安居客 2-既有安居客又有58同城\n"%opt))
-        if usertype not in [1, 2]:
-            raise ValueError("用户类型错误！")
-
-        if opt_id == 1:
-            '''新增'''
-            user.insert_user(username, password, usertype)
-        elif opt_id == 2:
-            '''修改'''
-            user.update_user(username, password, usertype)
         else:
-            raise ValueError("没有这个操作")
+            password = input("请输入需要%s操作的密码\n"%opt)
+            usertype = int(input("请输入需要%s操作的用户类型 1-只有安居客 2-既有安居客又有58同城\n"%opt))
+            if usertype not in [1, 2]:
+                raise ValueError("用户类型错误！")
 
+            if opt_id == 1:
+                '''新增'''
+                user.insert_user(username, password, usertype)
+            elif opt_id == 2:
+                '''修改'''
+                user.update_user(username, password, usertype)
+            else:
+                raise ValueError("没有这个操作")
     except ValueError:
         raise
     except Exception as e:
@@ -44,7 +42,7 @@ if __name__ == '__main__':
         if argv[1].strip() == "user":
             while True:
                 opt_id = int(input("请输入需要进行的用户操作：\n【0】退出程序\n【1】新增用户\n【2】修改用户\n【3】查看用户\n\n"))
-                if opt_id > 0 and opt_id < 3:
+                if opt_id > 0 and opt_id <= 3:
                     user_cmd(opt_id)
                 elif opt_id == 0:
                     break
