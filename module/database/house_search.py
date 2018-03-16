2572724919d5063467a675f25e741854ef3bfcfb
TauWoo <tauwoo@seuxw.cc>
2018-03-16 13:57:10 +0800

    随机图片返回策略

--- module/database/house_search.py
+++ module/database/house_search.py
@@ -16,6 +16,7 @@ class HouseSearch(DBController):
     @property
     def __search_house_list__(self):
         from .sql_template import house_search_sql
+        from random import sample
         
         limit_str = "limit %d"%(5*self.size)    # 从最多五倍size的数据里面抽取size数量的房源
         where_str = "where store = '%s' and house_type = %d and source = %d"%(self.store, self.housetype, self.source)
@@ -27,4 +28,10 @@ class HouseSearch(DBController):
         except Exception as e:
             db_fatal("搜索房源错误！")
             raise
-        self.house_list = [list(house_info) for house_info in self.cur.fetchall()]
+        
+        db_rtn = [list(house_info) for house_info in self.cur.fetchall()]
+
+        if len(db_rtn) <= 2*self.size:
+            self.house_list = db_rtn
+        else:
+            self.house_list = sample(db_rtn, 2*self.size)
